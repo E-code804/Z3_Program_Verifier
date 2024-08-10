a2e8a145c6f66302edac6df3c075cc5c8214788d
Erik Pfeffer <epfeffer@terpmail.umd.edu>
2024-08-10 10:35:27 -0400

    feat: Introduce newtype MyIntPair to avoid overlapping Arbitrary instances.
    - Created a MyIntPair newtype wrapper around (Int, Int) to define a custom Arbitrary instance without conflicting with QuickCheck's default instance.
    - Updated property testing code to use MyIntPair, ensuring valid inputs for IntDiv properties.

--- src/PBT.hs
+++ src/PBT.hs
@@ -32,7 +32,8 @@ import Test.QuickCheck
     (==>),
     (===),
     collect,
-    genericShrink
+    genericShrink,
+    suchThat
   )
 
 add1 :: Int -> Int -> Int
@@ -41,3 +42,31 @@ add1 n1 n2 = n1 + n2
 -- Correct way of asserting a property for quickChecking the reversal of a list
 prop_Reverse :: [Int] -> Property
 prop_Reverse xs = reverse (reverse xs) === xs
+
+-- Basic Testing for IntDiv.dfy
+
+-- This arbitrary instance did not work because it match with another arbitrary
+-- instance 'instance (Arbitrary a, Arbitrary b) => Arbitrary (a, b)'
+-- This generates a positive integer for `n` and any integer for `m`
+-- instance Arbitrary (Int, Int) where
+--   arbitrary = do
+--     m <- arbitrary         -- m can be any integer
+--     n <- arbitrary `suchThat` (> 0)  -- n must be positive
+--     return (m, n)
+
+-- Needed to make a newtype wrapper to avoid conflict.
+newtype MyIntPair = MyIntPair (Int, Int) deriving Show
+
+instance Arbitrary MyIntPair where
+  arbitrary = do
+    m <- arbitrary
+    n <- arbitrary `suchThat` (> 0)
+    return $ MyIntPair (m, n)
+
+-- Property using the newtype
+prop_IntDivValidInputs :: MyIntPair -> Bool
+prop_IntDivValidInputs (MyIntPair (m, n)) =
+  n > 0 && (m == (d * n + r)) && (0 <= r) && (r < n)
+  where
+    d = m `div` n
+    r = m `mod` n
